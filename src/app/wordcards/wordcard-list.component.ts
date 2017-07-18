7b91fb6d29311f232247187ceccda6d8ace2f45e
Vasco Correia <vasco.correia@netum.fi>
2017-07-18 12:55:53 +0300

    added meta tags for main page

--- src/app/wordcards/wordcard-list.component.ts
+++ src/app/wordcards/wordcard-list.component.ts
@@ -3,6 +3,7 @@ import {Observable} from "rxjs";
 import {Wordcard} from "./wordcard.model";
 import {Store} from "@ngrx/store";
 import {loadWordCards} from "./wordcard.actions";
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   moduleId: '' + module.id,
@@ -13,8 +14,14 @@ export class WordCardListComponent implements OnInit {
 
   cards: Observable<Wordcard[]>;
 
-  constructor(private store: Store<{}>) {
-  }
+  constructor(private store: Store<{}>, meta:Meta, title:Title) {
+    title.setTitle('My Fancy Wordgame');
+    meta.addTags([
+      {name: 'author', content: 'Vasco Correia'},
+      {name: 'keywords', content: 'learning, writing, kids, preschoolers'},
+      {name: 'description', content: 'A Word game for kids'}
+    ]);
+  };
 
   ngOnInit() {
     this.store.dispatch(loadWordCards());
